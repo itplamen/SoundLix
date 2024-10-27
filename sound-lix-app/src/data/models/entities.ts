aff077897a77753c30c970df92e2c80a12d0be8f
itplamen <itplamen@gmail.com>
2024-10-27 14:55:09 +0200

    Uninstalled cheerio. Removed unnecessary logic

--- sound-lix-app/src/data/models/entities.ts
+++ sound-lix-app/src/data/models/entities.ts
@@ -1,20 +1,3 @@
 export interface Entity {
   id: number;
 }
-
-export interface Genre extends Entity {
-  name: string;
-}
-
-export interface Artist extends Entity {
-  name: string;
-  pic: string;
-}
-
-export interface Song extends Entity {
-  title: string;
-  artistId: number;
-  genreId: number;
-  time: string;
-  isFeatured: boolean;
-}
