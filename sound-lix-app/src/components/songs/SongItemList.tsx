b1be10f9ac51d9b9284eff31e2febbf9056577fc
itplamen <itplamen@gmail.com>
2024-10-27 23:36:57 +0200

    Created song components

--- sound-lix-app/src/components/songs/SongItemList.tsx
+++ sound-lix-app/src/components/songs/SongItemList.tsx
@@ -1,4 +1,4 @@
-import { Song } from "@/app/data/models/entities";
+import { Song } from "@/models/data";
 import SongItem from "./SongItem";
 
 type Props = {
@@ -7,12 +7,24 @@ type Props = {
 
 const SongItemList = ({ songs }: Props) => {
   return (
-    <div className="p-8 sm:ml-64 mt-5">
-      <div className="rounded-lg dark:border-gray-700">
-        <div className="grid grid-cols-5 gap-4 mb-4">
-          {songs.map((song: Song) => (
-            <SongItem key={song.id} song={song} />
-          ))}
+    <div className="p-8 sm:ml-44">
+      <div className="grid rounded-lg dark:border-gray-700">
+        <div className="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <div className="flex items-center justify-between mb-4">
+            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+              Top Songs
+            </h5>
+          </div>
+          <div className="flow-root">
+            <ul
+              role="list"
+              className="divide-y divide-gray-200 dark:divide-gray-700"
+            >
+              {songs.map((song: Song) => (
+                <SongItem key={song.id} song={song} />
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
