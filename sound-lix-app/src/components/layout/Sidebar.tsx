443146fdd4f940d6c2ea6c89fac900e2c9b0d36f
itplamen <itplamen@gmail.com>
2025-06-04 01:09:05 +0300

    Created icon types

--- sound-lix-app/src/components/layout/Sidebar.tsx
+++ sound-lix-app/src/components/layout/Sidebar.tsx
@@ -1,3 +1,12 @@
+import { COLOR } from "@/utils/constants";
+import Icon from "../Icons/Icon";
+import HomeIconType from "../Icons/Types/HomeIconType";
+import ProfileIconType from "../Icons/Types/ProfileIconType";
+import GitHubIconType from "../Icons/Types/GitHubIconType";
+import SignInIconType from "../Icons/Types/SignInIconType";
+import RadioIconType from "../Icons/Types/RadioIconType";
+import SongIconType from "../Icons/Types/SongIconType";
+
 type Item = {
   name: string;
   url: string;
@@ -10,79 +19,35 @@ const getItems = (): Item[][] => {
       {
         name: "Home",
         url: "/",
-        icon: (
-          <path
-            fillRule="evenodd"
-            d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z"
-            clipRule="evenodd"
-          />
-        ),
+        icon: <HomeIconType />,
       },
       {
         name: "Songs",
         url: "#",
-        icon: (
-          <path
-            fillRule="evenodd"
-            d="M17.316 4.052a.99.99 0 0 0-.9.14c-.262.19-.416.495-.416.82v8.566a4.573 4.573 0 0 0-2-.464c-1.99 0-4 1.342-4 3.443 0 2.1 2.01 3.443 4 3.443 1.99 0 4-1.342 4-3.443V6.801c.538.5 1 1.219 1 2.262 0 .56.448 1.013 1 1.013s1-.453 1-1.013c0-1.905-.956-3.18-1.86-3.942a6.391 6.391 0 0 0-1.636-.998 4 4 0 0 0-.166-.063l-.013-.005-.005-.002h-.002l-.002-.001ZM4 5.012c-.552 0-1 .454-1 1.013 0 .56.448 1.013 1 1.013h9c.552 0 1-.453 1-1.013 0-.559-.448-1.012-1-1.012H4Zm0 4.051c-.552 0-1 .454-1 1.013 0 .56.448 1.013 1 1.013h9c.552 0 1-.454 1-1.013 0-.56-.448-1.013-1-1.013H4Zm0 4.05c-.552 0-1 .454-1 1.014 0 .559.448 1.012 1 1.012h4c.552 0 1-.453 1-1.012 0-.56-.448-1.013-1-1.013H4Z"
-            clipRule="evenodd"
-          />
-        ),
+        icon: <SongIconType />,
       },
       {
         name: "Radio",
         url: "#",
-        icon: (
-          <>
-            <path
-              fillRule="evenodd"
-              d="M5 8a1 1 0 0 1 1 1v3a4.006 4.006 0 0 0 4 4h4a4.006 4.006 0 0 0 4-4V9a1 1 0 1 1 2 0v3.001A6.006 6.006 0 0 1 14.001 18H13v2h2a1 1 0 1 1 0 2H9a1 1 0 1 1 0-2h2v-2H9.999A6.006 6.006 0 0 1 4 12.001V9a1 1 0 0 1 1-1Z"
-              clipRule="evenodd"
-            />
-            <path d="M7 6a4 4 0 0 1 4-4h2a4 4 0 0 1 4 4v5a4 4 0 0 1-4 4h-2a4 4 0 0 1-4-4V6Z" />
-          </>
-        ),
+        icon: <RadioIconType />,
       },
     ],
     [
       {
         name: "Sign In",
         url: "#",
-        icon: (
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
-          />
-        ),
+        icon: <SignInIconType />,
       },
       {
         name: "Sign Up",
         url: "#",
-        icon: (
-          <>
-            <path
-              fillRule="evenodd"
-              d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
-              clipRule="evenodd"
-            />
-          </>
-        ),
       },
     ],
     [
       {
         name: "GitHub",
         url: "https://github.com/itplamen/SoundLix",
-        icon: (
-          <path
-            fillRule="evenodd"
-            d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z"
-            clipRule="evenodd"
-          />
-        ),
+        icon: <GitHubIconType />,
       },
     ],
   ];
@@ -112,17 +77,9 @@ const Sidebar = () => {
                   href={item.url}
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
-                  <svg
-                    className="w-[25px] h-[25px] text-gray-800 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
+                  <Icon size={25} color={COLOR.DARK_GRAY}>
                     {item.icon}
-                  </svg>
+                  </Icon>
                   <span className="flex-1 ms-3 whitespace-nowrap">
                     {item.name}
                   </span>
